e271beb454ff035d1204ba85f5859102146b47b2
maksymchak <maksym4ak@gmail.com>
2018-10-24 21:51:09 +0300

    add jquery

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import $ from 'jquery';
 import { Greeter } from './greeter.js';
 
 const greeter = new Greeter();
 
 const message = greeter.greet('Hello', 'Webpack');
 
-console.log(message);
+$('body').text(message);
